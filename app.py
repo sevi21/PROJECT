350d2683368071d5fda0a9eea1bc79a12410a34e
sevi21 <maj.savorgnani@gmail.com>
2025-03-26 13:27:20 +0100

    frontend -80% done

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/qrcode')
+def qrcode():
+    return render_template('qrcode.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
